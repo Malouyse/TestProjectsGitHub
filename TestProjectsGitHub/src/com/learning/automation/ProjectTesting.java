a08a6c215eb7f9a6ce7806d1ef2ae9f40fc5633f
Judith Aio <Judith Aio@DESKTOP-KSD455U>
2019-03-07 00:55:25 -0500

    New Project Codes

---
+++ TestProjectsGitHub/src/com/learning/automation/ProjectTesting.java
@@ -0,0 +1,13 @@
+package com.learning.automation;
+
+public class ProjectTesting {
+	public static void main(String[] args) {
+		
+		int mel =5;
+		int meli =7;
+		int melia = 8;
+		int me =0;
+	}
+	
+
+}
